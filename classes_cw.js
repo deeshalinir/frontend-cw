3f26e9ed8f5bca694e556f1628b9dca77b3712ba
Deeshalini Ramgoolam <dr721@live.mdx.ac.uk>
2024-11-19 11:14:22 +0400

    updated classes_cw.js

---
+++ classes_cw.js
@@ -0,0 +1,83 @@
+let classes = [ 
+    {
+        id: 1,
+        iconClass:"mathematics.png",
+        subject: "Mathematics",
+        location: "Hendon",
+        price: 23.00,
+        spaces: 10
+    },
+    {
+        id: 2,
+        iconClass: "coding.png",
+        subject: "Computer Science",
+        location: "Mont Rose",
+        price: 27.00,
+        spaces: 5
+        
+    },
+    {
+        id: 3,
+        iconClass: "art.png",
+        subject: "Art",
+        location: "Hammersmith",
+        price: 25.00,
+        spaces: 5
+    },
+    {
+        id: 4,
+        iconClass: "geography.png",
+        subject: "Geography",
+        location: "Collindate",
+        price: 17.00,
+        spaces: 5
+    },
+    {
+        id: 5,
+        iconClass: "science.png",
+        subject: "Sciences",
+        location: "Edgeware",
+        price: 24.00,
+        spaces: 5    
+    },
+    {
+        id: 6,
+        iconClass: "business.png",
+        subject: "Business Entrepreneuship",
+        location: "Hyde",
+        price: 29.00,
+        spaces: 5
+    },
+    {
+        id: 7,
+        iconClass: "cycling.png",
+        subject: "Cycling Club",
+        location: "Petersborogh",
+        price: 10.00,
+        spaces: 5
+    },
+    {
+        id: 8,
+        iconClass: "psycology.png",
+        subject: "Psychology",
+        location: "St Mary",
+        price: 33.00,
+        spaces: 5
+    },
+    {
+        id: 9,
+        iconClass: "chinese.png",
+        subject: "Chinese",
+        location: "Florence",
+        price: 25.00,
+        spaces: 5
+    },
+    {
+        id: 10,
+        iconClass: "debate.png",
+        subject: "Debate Club",
+        location: "Glebe",
+        price: 8.00,
+        spaces: 5
+    }
+]
